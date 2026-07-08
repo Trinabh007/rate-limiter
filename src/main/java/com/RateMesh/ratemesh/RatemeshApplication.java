8ac8faf17d279e8233b9d1df94a3903fcae45129
Trinabh <1by23cs275@bmsit.in>
2026-07-08 17:30:42 +0530

    feat: register interceptor, exclude /admin/** from rate limiting

---
+++ src/main/java/com/RateMesh/ratemesh/RatemeshApplication.java
@@ -0,0 +1,13 @@
+package com.RateMesh.ratemesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatemeshApplication.class, args);
+	}
+
+}
